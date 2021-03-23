8158653ddc967f66ea4bba64fd885f86ae171da1
Mehmet Çakmaktaşı <42716195+daddydemir@users.noreply.github.com>
2021-03-23 21:26:27 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,77 @@
+import socket 
+import threading
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+Style.RESET_ALL
+
+def kullaniciAdi():
+	nick = ""
+	while True:
+		nick = input("Lütfen bir nickname giriniz : ")
+		if(nick == ""):
+			print("geçersiz !")
+			continue
+		if(len(nick) > 10):
+			print("çok uzun oldu !")
+		else:
+			break
+	return nick
+
+def checkMsg(gelen):
+	isim = ""
+	mesaj = ""
+	sayac = 0
+	isUser = False
+	Style.RESET_ALL
+	
+	for i in gelen:
+		sayac += 1
+		if(i == "#"):
+			# kullanıcılardan gelen mesajda # vardır 
+			isUser = True
+			mesaj = gelen[sayac:len(gelen)]
+			break
+		isim += i
+			
+			
+	if(isUser == False):
+		print(Fore.RED + gelen +Fore.WHITE)
+	else:
+		Style.RESET_ALL
+		print(Fore.YELLOW + isim + Style.RESET_ALL + " >>> " + Fore.BLUE  + mesaj + Fore.WHITE)
+	
+	Style.RESET_ALL
+		
+def getMessage():
+	while True:
+		try:
+			response = clientSocket.recv(1024) # sunucudan mesaj geliyor .:.
+		except:
+			a =""
+		msg = response.decode('utf-8')
+		#print(msg+"\n")
+		checkMsg(msg)
+
+
+isim = kullaniciAdi()
+
+clientSocket = socket.socket() # default olarak IPv4 ü kullanıyor .:. 
+host = "91.151.93.119"
+port = 4545
+
+print("Waiting for Connection")
+try:
+	clientSocket.connect((host,port)) # belirtilen porta bağlanma isteği gönderiliyor .:.
+	clientSocket.send(str.encode(isim))
+except socket.error as e:
+	print(str(e))
+
+t1 = threading.Thread(target=getMessage)
+t1.start()
+while True:
+	Input = input()
+	clientSocket.send(str.encode(Input))
+
+clientSocket.close()
---
+++ server.py
@@ -0,0 +1,76 @@
+import socket
+from _thread import * 
+
+
+userList  = {} # ip : isim
+clientLis = [] # client 
+
+# host : port : socket 
+
+host = "localhost" # sunucunu ip adresini yaz
+port = 4545        # isteğe bağlı 
+s = socket.socket()
+
+# connect and listen port
+
+try:
+	s.bind((host , port))
+	s.listen()
+except:
+	print("hata - 1")
+	
+
+print("Bağlantı bekleniyor . . .")
+
+# create thread function 
+
+def broadCast(kullanici , number):
+	name = kullanici.recv(1024)
+	userList[number] = name.decode('utf-8')
+	oturumMesaj = userList[number]+" odaya katıldı "
+	
+	f = open("login.txt" , "a" , encoding='utf-8')
+	yaz = userList[number]+" : " + iport[0]+"\n"
+	f.write(yaz)
+	f.close()
+	
+	for i in clientLis:
+		i.send(oturumMesaj.encode())
+		
+	# odaya katılan kullanıcının ismini herkese gönder 
+	
+	while True:
+		try:
+			gelen = kullanici.recv(1024)
+			gelen = gelen.decode('utf-8')
+			
+			# bu mesajı bütün kullanıcılara gönderelim 
+			# kimden geldiği bilgisini de ekleyelim 
+			gelen = userList[number]+"#"+gelen
+			
+			for i in clientLis:
+				i.send(gelen.encode())
+		except:
+			# hata almışsam kullanici nın bağlantısı kopmuştur .
+			# clientLis den silmem gerekli
+			clientLis.remove(kullanici)
+			# ve bu process in sonlanabilmesi için döngüden çıkmam gerekir
+			# çıkmadan önce diğer kullanıcılara kimin çıktığını söylemeliyim 
+			gonder = userList[number]+" odadan ayrıldı"
+			for i in clientLis:
+				i.send(gonder.encode())
+			break
+		
+
+# finish function
+
+while True:
+	istemci , iport = s.accept() # gelen bağlantı isteğini kabul ediyor 
+	clientLis.append(istemci)
+	print(iport[0] , " bağlandı") # localde çalıştığı için port numarası 
+	start_new_thread(broadCast , (istemci , iport[0] ,)  )
+	
+# 
+# github.com/daddydemir
+
+
